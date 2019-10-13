227366f6d8c654a4d08b9564c4795ada72b6dfc1
Mikko Metso <mikko.metso@sok.fi>
2019-10-13 15:38:59 +0300

    added missing subtitle

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,9 +8,10 @@ class NotFoundPage extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
+    const siteSubTitle = data.site.siteMetadata.subtitle
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <Layout location={this.props.location} title={siteTitle} subtitle={siteSubTitle}>
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
